636daa139aff9d1cde903856ed71f5f10f8510e5
Maria Voronina <maria_voronina@epam.com>
2019-10-21 16:30:50 +0300

    add project

---
+++ project/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app:  ["./src/index.js"]
+    },
+    output: {
+        filename: "[name].js",
+        path: path.join(__dirname, 'dist'),
+    },
+    devServer: {
+       hot: true,
+       headers: {
+        'Access-Control-Allow-Origin': '*'
+    }
+    },
+    mode:'development',
+    module: {
+             rules: [
+                {
+                    test: /\.js[x]?$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
+                }
+             ],
+            },
+            plugins: [
+                new HtmlWebpackPlugin()
+            ],
+            resolve: {
+                extensions: [ ".js", ".jsx" ]
+            }
+};
+        
